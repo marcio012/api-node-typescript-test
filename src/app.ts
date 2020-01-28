26cbcfb8d354f0deffdbd79d05148218e6def333
marcio heleno <marcio012@edu.unifor.br>
2020-01-28 05:12:11 -0300

    integracao continua travis ci

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import { APIRoute } from './routes/api';
@@ -10,9 +11,17 @@ class App {
   public userRoutes: UserRoute = new UserRoute();
   public apiRoutes: APIRoute = new APIRoute();
   public orderRoutes: OrderRoute = new OrderRoute();
-  public mongoUrl: string = 'mongodb://localhost/order-api';
+  public mongoUrl: string;
+  public mongoUser: string;
+  public mongoPass: string;
 
   constructor() {
+    const path = `${__dirname}/../.env.${process.env.NODE_ENV}`;
+    dotenv.config({ path: path });
+    this.mongoUrl = `mongodb://${process.env.MONGODB_URL_PORT}/${process.env.MONGODB_DATABASE}`;
+    this.mongoUser = `${process.env.MONGODB_USER}`;
+    this.mongoPass = `${process.env.MONGODB_PASS}`;
+
     this.app = express();
     this.app.use(bodyParser.json());
     this.userRoutes.routes(this.app);
@@ -22,6 +31,20 @@ class App {
   }
 
   private mongoSetup(): void {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    let options;
+
+    if (process.env.NODE_ENV !== 'prod') {
+      options = {
+        useNewUrlParser: true,
+      };
+    } else {
+      options = {
+        user: this.mongoUser,
+        pass: this.mongoPass,
+        useNewUrlParser: true,
+      };
+    }
     mongoose
       .connect(this.mongoUrl, {
         useUnifiedTopology: true,
